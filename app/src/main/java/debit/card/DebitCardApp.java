d7567c85aded68eca19e7fe17157b67bcae7006c
Arkadiusz Socha <socha.arkadiusz@gmail.com>
2023-09-16 10:41:49 +0200

    feat: Added simple spring boot application, and dockerize it

---
+++ app/src/main/java/debit/card/DebitCardApp.java
@@ -0,0 +1,12 @@
+package debit.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitCardApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebitCardApp.class, args);
+    }
+}
---
+++ app/src/test/kotlin/debit/card/DebitCardAppTest.kt
@@ -0,0 +1,11 @@
+package debit.card
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class DebitCardAppTest {
+    @Test
+    fun appHasAGreeting() {
+        assertTrue(true)
+    }
+}
